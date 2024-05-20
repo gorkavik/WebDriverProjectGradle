9abeff24284a4d01475e91b1c9a458101dc9bc50
Горкавик Ольга Ивановна <OGorkavik@alfabank.ru>
2024-05-20 18:24:51 +0300

    Project created

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,9 +1,10 @@
 package org.example;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,7 @@ public class LoginTest {
     public static LoginPage loginPage;
     public static WebDriver driver;
 
-    @BeforeClass
+    @BeforeTest
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
         driver = new ChromeDriver();
@@ -23,15 +24,13 @@ public class LoginTest {
     }
 
     @Test
-    public void loginTest() {
+    public void validLoginTest() {
         loginPage.inputLogin(ConfProperties.getProperty("login"));
         loginPage.inputPasswd(ConfProperties.getProperty("password"));
         loginPage.clickLoginBtn();
-
     }
-
-    @AfterClass
-    public static void tearDown() throws InterruptedException{
+    @AfterTest
+    public static void tearDown() throws InterruptedException {
         Thread.sleep(3000);
         driver.quit(); }
 }
